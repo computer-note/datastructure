0dc8a5b3358377534f2ded00fca452b6f4f524b5
computer-note <planomenes@gmail.com>
2024-05-30 18:39:24 +0900

    [수정] 1. insertionPointIndex 변수명을 currentLength로 변경 2. stringify 메서드 내에서 배열 요소를 문자열로 바꿀 때 JSON.stringify 메서드 사용

--- FixedArray/FixedArray.js
+++ FixedArray/FixedArray.js
@@ -1,62 +1,75 @@
 class FixedArray {
-   #array = [];
-   #arrayLength = 0;
-   #insertionPointIndex = 0;
+   _array = [];
+   _arrayLength = 0;
+   _currentLength = 0;
 
-   constructor(size) {
-      if (size < 0) {
-         throw new Error('배열 길이는 0이상 이어야 합니다.');
-      }
+   constructor(length) {
+      this._arrayLength = length;
 
-      this.#arrayLength = size;
-
-      for (let i = 0; i < this.#arrayLength; ++i) {
-         this.#array[i] = undefined;
+      for (let i = 0; i < length; ++i) {
+         this._array[i] = undefined;
       }
    }
 
-   push(item) {
-      if (this.#insertionPointIndex >= this.#arrayLength) {
+   push(element) {
+      if (this._currentLength >= this._arrayLength) {
          throw new Error('배열이 꽉 찼습니다.');
       }
-      this.#array[this.#insertionPointIndex] = item;
-      ++this.#insertionPointIndex;
+
+      const insertionPointIdx = this._currentLength;
+      this._array[insertionPointIdx] = element;
+
+      ++this._currentLength;
    }
 
    pop() {
-      if (this.#insertionPointIndex <= 0) {
-         throw new Error('배열이 비었습니다.');
+      if (this._currentLength <= 0) {
+         throw new Error('배열이 비었습니다');
       }
 
-      const item = this.#array[this.#insertionPointIndex - 1];
-      --this.#insertionPointIndex;
-      return item;
+      const topElemIdx = this._currentLength - 1;
+
+      const element = this._array[topElemIdx];
+      this._array[topElemIdx] = undefined;
+
+      --this._currentLength;
+
+      return element;
    }
 
-   arrayLength() {
-      return this.#insertionPointIndex;
+   getLength() {
+      return this._currentLength;
    }
 
    stringify() {
-      let string = '';
-
-      string += '[';
-      for (let i = 0; i < this.#insertionPointIndex; ++i) {
-         string += this.#array[i];
-         if (i !== this.#insertionPointIndex - 1) {
-            string += ', ';
+      let str = '';
+      str += '[';
+      for (let i = 0; i < this._currentLength; ++i) {
+         str += JSON.stringify(this._array[i]);
+         if (i !== this._currentLength - 1) {
+            str += ', ';
          }
       }
-      string += ']';
-
-      return string;
+      str += ']';
+      return str;
    }
 }
 
-/*
-프로퍼티: #array, #arrayLength
-  #array 초기값: undefined
-메서드: push, pop, getLength, stringify
+/* V1 Todo
+    constructor() { }
+
+    push(element) { }
+      배열 맨 뒤에 요소 추가
+      배열의 길이가 _arrayLength를 초과할 경우 요소를 추가되면 안됨.
+
+    pop() { }
+      배열의 맨 마지막 요소를 제거하고 그 요소를 반환
+
+    getLength() { }
+      현재 배열의 사용되고 있는 크기를 반환
+
+    stringify() { }
+      현재 배열의 상태를 string으로 반환
 */
 
 module.exports = FixedArray;
