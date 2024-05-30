6c3e0146f06041bd2af30cd6f3829148a8222717
computer-note <planomenes@gmail.com>
2024-05-30 19:12:18 +0900

    [수정] 베이스클래스 FixedArray의 프로퍼티를 private 필드를 사용하도록 수정

--- FixedArray/FixedArray.js
+++ FixedArray/FixedArray.js
@@ -1,57 +1,61 @@
 class FixedArray {
-   _array = [];
-   _arrayLength = 0;
-   _currentLength = 0;
+   #array = [];
+   #arrayLength = 0;
+   #currentLength = 0;
 
    constructor(length) {
-      this._arrayLength = length;
+      this.#arrayLength = length;
 
       for (let i = 0; i < length; ++i) {
-         this._array[i] = undefined;
+         this.#array[i] = undefined;
       }
    }
 
    push(element) {
-      if (this._currentLength >= this._arrayLength) {
+      if (this.#currentLength >= this.#arrayLength) {
          throw new Error('배열이 꽉 찼습니다.');
       }
 
-      const insertionPointIdx = this._currentLength;
-      this._array[insertionPointIdx] = element;
+      const insertionPointIdx = this.#currentLength;
+      this.#array[insertionPointIdx] = element;
 
-      ++this._currentLength;
+      ++this.#currentLength;
    }
 
    pop() {
-      if (this._currentLength <= 0) {
+      if (this.#currentLength <= 0) {
          throw new Error('배열이 비었습니다');
       }
 
-      const topElemIdx = this._currentLength - 1;
+      const topElemIdx = this.#currentLength - 1;
 
-      const element = this._array[topElemIdx];
-      this._array[topElemIdx] = undefined;
+      const element = this.#array[topElemIdx];
+      this.#array[topElemIdx] = undefined;
 
-      --this._currentLength;
+      --this.#currentLength;
 
       return element;
    }
 
    getLength() {
-      return this._currentLength;
+      return this.#currentLength;
    }
 
    stringify() {
       let str = '';
       str += '[';
-      for (let i = 0; i < this._currentLength; ++i) {
-         str += JSON.stringify(this._array[i]);
-         if (i !== this._currentLength - 1) {
+      for (let i = 0; i < this.#currentLength; ++i) {
+         str += JSON.stringify(this.#array[i]);
+         if (i !== this.#currentLength - 1) {
             str += ', ';
          }
       }
       str += ']';
       return str;
+   }
+
+   at(index) {
+      return this.#array[index];
    }
 }
 
@@ -60,7 +64,7 @@ class FixedArray {
 
     push(element) { }
       배열 맨 뒤에 요소 추가
-      배열의 길이가 _arrayLength를 초과할 경우 요소를 추가되면 안됨.
+      배열의 길이가 #arrayLength를 초과할 경우 요소를 추가되면 안됨.
 
     pop() { }
       배열의 맨 마지막 요소를 제거하고 그 요소를 반환
