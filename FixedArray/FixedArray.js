22497acda5958dc7f4407b083f86efbf29cd6cef
computer-note <planomenes@gmail.com>
2024-06-28 09:42:34 +0900

    update (FixedArray.js): 확장 클래스를 위한 메서드 추가
    확장 클래스에서 FixedArray의 내부 배열을 수정할 수 있는 메서드,
    내부 배열에 대한 정보에 접근할 수 있는 메서드를 제공

--- FixedArray/FixedArray.js
+++ FixedArray/FixedArray.js
@@ -1,62 +1,74 @@
 class FixedArray {
-   #array = [];
-   #arrayLength = 0;
-   #currentLength = 0;
+  #array = [];
+  #arrayLength = 0;
+  #currentLength = 0;
 
-   constructor(length) {
-      this.#arrayLength = length;
+  constructor(length) {
+    this.#arrayLength = length;
 
-      for (let i = 0; i < length; ++i) {
-         this.#array[i] = undefined;
+    for (let i = 0; i < length; ++i) {
+      this.#array[i] = undefined;
+    }
+  }
+
+  push(element) {
+    if (this.#currentLength >= this.#arrayLength) {
+      throw new Error('배열이 꽉 찼습니다.');
+    }
+
+    const insertionPointIdx = this.#currentLength;
+    this.#array[insertionPointIdx] = element;
+
+    ++this.#currentLength;
+  }
+
+  pop() {
+    if (this.#currentLength <= 0) {
+      throw new Error('배열이 비었습니다');
+    }
+
+    const topElemIdx = this.#currentLength - 1;
+
+    const element = this.#array[topElemIdx];
+    this.#array[topElemIdx] = undefined;
+
+    --this.#currentLength;
+
+    return element;
+  }
+
+  getLength() {
+    return this.#currentLength;
+  }
+
+  stringify() {
+    let str = '';
+    str += '[';
+    for (let i = 0; i < this.#currentLength; ++i) {
+      str += JSON.stringify(this.#array[i]);
+      if (i !== this.#currentLength - 1) {
+        str += ', ';
       }
-   }
+    }
+    str += ']';
+    return str;
+  }
 
-   push(element) {
-      if (this.#currentLength >= this.#arrayLength) {
-         throw new Error('배열이 꽉 찼습니다.');
-      }
+  at(index) {
+    return this.#array[index];
+  }
 
-      const insertionPointIdx = this.#currentLength;
-      this.#array[insertionPointIdx] = element;
+  writeAt(index, element) {
+    this.#array[index] = element;
+  }
 
-      ++this.#currentLength;
-   }
+  setLength(newLength) {
+    this.#currentLength = newLength;
+  }
 
-   pop() {
-      if (this.#currentLength <= 0) {
-         throw new Error('배열이 비었습니다');
-      }
-
-      const topElemIdx = this.#currentLength - 1;
-
-      const element = this.#array[topElemIdx];
-      this.#array[topElemIdx] = undefined;
-
-      --this.#currentLength;
-
-      return element;
-   }
-
-   getLength() {
-      return this.#currentLength;
-   }
-
-   stringify() {
-      let str = '';
-      str += '[';
-      for (let i = 0; i < this.#currentLength; ++i) {
-         str += JSON.stringify(this.#array[i]);
-         if (i !== this.#currentLength - 1) {
-            str += ', ';
-         }
-      }
-      str += ']';
-      return str;
-   }
-
-   at(index) {
-      return this.#array[index];
-   }
+  getCapacity() {
+    return this.#arrayLength;
+  }
 }
 
 /* V1 Todo
